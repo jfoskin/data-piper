daed9c8eb9e467a173f9ad8becea3f5d6c3319d5
Jazma Foskin <82051355+jfoskin@users.noreply.github.com>
2022-07-07 11:33:26 -0500

    Sequelize model created

---
+++ server/db/index.js
@@ -0,0 +1,49 @@
+const Sequelize = require('sequelize');
+
+const dbConnection = new Sequelize(`postgres://localhost:5432/data-piper`);
+
+module.exports = dbConnection;
+
+const Client = dbConnection.define('Client', {
+    Client: {
+		type: Sequelize.STRING(50),
+		allowNull: false,
+		validate: {
+			notEmpty: true,
+		},
+	},
+	POC: {
+		type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+			notEmpty: true,
+		},
+	},
+	Email: {
+		type: Sequelize.STRING,
+		validate: {
+			isEmail: true,
+            notEmpty: true
+		},
+	},
+	Role: {
+		type: Sequelize.STRING,
+		allowNull: false,
+	},
+	Urgency: {
+		type: Sequelize.ENUM(['High', 'Med', 'Low']),
+		allowNull: false,
+		defaultValue: 'High',
+		validate: {
+			isIn: [['High', 'Med', 'Low']],
+		},
+	},
+    Quantity: {
+        type: Sequelize.FLOAT,
+		defaultValue: 1,
+		validate: {
+			min: 0,
+			max: 100,
+		},
+    },
+})
--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
+const dbConnection = require('./db');
+const PORT = 8080;
 
-const PORT = 8080
-app.listen(PORT, ()=> {
-    console.log(`Server is running on port ${PORT}`)
-})
+
+const startServer = async () => {
+    dbConnection.sync();
+    app.listen(PORT, ()=> {
+        console.log(`Server is running on port ${PORT}`);
+    });
+};
+
+startServer();
 
 app.get("/", (req, res) => {
-    res.send('Hello World')
-})
+    res.send('Hello World');
+});
