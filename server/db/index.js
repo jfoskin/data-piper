ab75b9d824310a906ed33a1cbe045b0da9c5157a
Jazma Foskin <82051355+jfoskin@users.noreply.github.com>
2022-07-09 11:35:44 -0500

    update 2 for db

--- server/db/index.js
+++ server/db/index.js
@@ -4,6 +4,22 @@ const chalk = require('chalk');
 
 const dbName = process.env.NODE_ENV === 'test' ? `${pkg.name}_test` : pkg.name;
 console.log(chalk.yellow(`Opening database connection to ${dbName}`));
+const config = {
+	logging: false
+  };
+  
+  if(process.env.LOGGING === 'true'){
+	delete config.logging
+  }
+  
+  
+  if(process.env.DATABASE_URL){
+	config.dialectOptions = {
+	  ssl: {
+		rejectUnauthorized: false
+	  }
+	};
+  }
 
 const dbConnection = new Sequelize( process.env.DATABASE_URL||`postgres://localhost:5432/${dbName}`, config);
 
